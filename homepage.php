58358574cb289659a7a8df7f5370ee07d33ae0c1
AyaHeshaam <96925793+AyaHeshaam@users.noreply.github.com>
2024-01-14 21:07:29 +0200

    Create homepage.php
    The Homepage of our web application.

---
+++ homepage.php
@@ -0,0 +1,110 @@
+<link rel="stylesheet" href="styles.css">
+<header>
+<button class="button" data-text="Awesome">
+
+    <span class="actual-text">&nbsp;StegOn&nbsp;</span>
+    <span aria-hidden="true" class="hover-text">&nbsp;Uncover Secrets&nbsp;</span>
+</button>
+<div class="navigation-card">
+    <a href="#" class="tab">
+      <svg
+        class="svgIcon"
+        viewBox="0 0 104 100"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100.5 40.75V96.5H66V68.5V65H62.5H43H39.5V68.5V96.5H3.5V40.75L52 4.375L100.5 40.75Z"
+          stroke="white"
+          stroke-width="7"
+        ></path>
+      </svg>
+    </a>
+  
+    <a href="#" class="tab">
+      <svg
+        width="104"
+        height="100"
+        viewBox="0 0 104 100"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="21.5"
+          y="3.5"
+          width="60"
+          height="60"
+          rx="30"
+          stroke="white"
+          stroke-width="7"
+        ></rect>
+        <g clip-path="url(#clip0_41_27)">
+          <mask
+            id="mask0_41_27"
+            style="mask-type:luminance"
+            maskUnits="userSpaceOnUse"
+            x="0"
+            y="61"
+            width="104"
+            height="52"
+          >
+            <path
+              d="M0 113C0 84.2812 23.4071 61 52.1259 61C80.706 61 104 84.4199 104 113H0Z"
+              fill="white"
+            ></path>
+          </mask>
+          <g mask="url(#mask0_41_27)">
+            <path
+              d="M-7 113C-7 80.4152 19.4152 54 52 54H52.2512C84.6973 54 111 80.3027 111 112.749H97C97 88.0347 76.9653 68 52.2512 68H52C27.1472 68 7 88.1472 7 113H-7ZM-7 113C-7 80.4152 19.4152 54 52 54V68C27.1472 68 7 88.1472 7 113H-7ZM52.2512 54C84.6973 54 111 80.3027 111 112.749V113H97V112.749C97 88.0347 76.9653 68 52.2512 68V54Z"
+              fill="white"
+            ></path>
+          </g>
+        </g>
+        <defs>
+          <clipPath id="clip0_41_27">
+            <rect
+              width="104"
+              height="39"
+              fill="white"
+              transform="translate(0 61)"
+            ></rect>
+          </clipPath>
+        </defs>
+      </svg>
+    </a>
+  
+   
+  </div>
+  
+
+
+</header>
+<body>
+  
+  <div class="animation">Sign In to Acccess our tool!</div>
+<div class="form-container">
+<form class="form">
+    <p class="form-title">Sign in to your account</p>
+     <div class="input-container">
+       <input type="email" placeholder="Enter email">
+       <span>
+       </span>
+   </div>
+   <div class="input-container">
+       <input type="password" placeholder="Enter password">
+     </div>
+      <button type="submit" class="submit">
+     Sign in
+   </button>
+
+   <p class="signup-link">
+     No account?
+     <a href="signup.html">Sign up</a>
+   </p>
+</form>
+</div>
+</body>
+
+
+    
+ 
